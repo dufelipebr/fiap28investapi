4b4487828fce8e4fdda4424c517f9cde29694e58
dufelipebr <du.felipe.br@gmail.com>
2024-08-09 16:04:35 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,9 +19,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
+
+
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "FIAP Investimento", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "invest", Version = "v1" });
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
